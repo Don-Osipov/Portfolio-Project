948977061a3d4f66f50ae2ea9a1735776826af34
Don Osipov <donosipov@gmail.com>
2019-07-29 11:53:23 -0400

    Implemented Babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import '@babel/polyfill';
+
 import * as final from './js/three';
 import './scss/main.scss';
 console.log('WEBPACK TEST');
 final.testpasses();
 console.log('hi');
+[1, 2, 3].map(n => n * 2);
--- webpack.prod.js
+++ webpack.prod.js
@@ -37,6 +37,11 @@ module.exports = merge(common, {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader, // 3. Extract css into files
